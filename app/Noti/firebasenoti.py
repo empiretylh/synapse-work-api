d9f329ef384a909c0143183a60601ec392f65b1e
empiretylh <thuralinhtut3@gmail.com>
2023-11-30 00:20:39 +0630

    firebase noti

--- app/Noti/firebasenoti.py
+++ app/Noti/firebasenoti.py
@@ -1,8 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials, messaging
 
-# Initialize Firebase Admin SDK
-cred = credentials.Certificate("synapsework_firebase.json")
+# Get the absolute path to the certificate file
+cert_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'synapsework_firebase.json')
+
+# Use the absolute path when creating the credentials
+cred = credentials.Certificate(cert_path)
 firebase_admin.initialize_app(cred)
 
 def send_multicast_message(device_tokens, message_title, message_body, custom_data=None):
