b446200528c650220ab74aa4f541eb2bca4b3130
empiretylh <thuralinhtut3@gmail.com>
2023-11-30 17:10:20 +0630

    update models

--- app/models.py
+++ app/models.py
@@ -90,10 +90,13 @@ class CourseRequest(models.Model):
     coursename = models.ForeignKey(Course, on_delete=models.CASCADE)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     confirm = models.BooleanField(default=False)
+    payment_amount = models.CharField(max_length=50,null=True, default='5000')
     joindate = models.DateTimeField(auto_now_add=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.user.username + " : " + self.coursename.course_name
 
 class Lessons(Content):
     #lesson_title = models.CharField(max_length=50)
